048d24520b6062fd314e72a52737c5cf17d0214a
Elias <elias.yter@gmail.com>
2021-10-10 01:20:06 +0200

    paswoord 255

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-import requests
+noe="$2b$12$VwAa1Mn889/W4cIQn5oxK.tkEO89ESejV1T6SbYWLM4TFv7BvNGri"
 
 
-key='29739b00f42ed462a2a850e5c34c03d9e65ae652'
-response = requests.get(f'https://api.nomics.com/v1/markets?key={key}')
 
-print(response.text)
+
+print(len(noe))
