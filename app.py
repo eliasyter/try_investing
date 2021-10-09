7ba3b6f0771269635e30336b6884fafb49691e38
Elias <elias.yter@gmail.com>
2021-10-10 01:14:38 +0200

    paswoord 255

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def load_user(user_id):
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key= True)
     username=db.Column(db.String(20),nullable=False, unique=True)
-    password= db.Column(db.String(80),nullable=False)
+    password= db.Column(db.String(255),nullable=False)
     #date_created does not work 
     # if i have time in a later time i want to add the time tha user was made
 class RegisterForm(FlaskForm):
