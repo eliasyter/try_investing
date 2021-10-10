62bd36f3818530b57f7b4ab3476bd0ae5f8b28de
Elias <elias.yter@gmail.com>
2021-10-10 15:43:31 +0200

    adding utf8 to password

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def SignUp():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         new_user=User(username=form.username.data,password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 noe="$2b$12$VwAa1Mn889/W4cIQn5oxK.tkEO89ESejV1T6SbYWLM4TFv7BvNGri"
 noe="$2b$12$N4ac2x2Z9kAkzfIANTAS.OXufCphXOsfMvraOQAu0hV5JpZl/xXCW"
-
+noe="$2b$12$ISXSm1EsklhOO7e.7A1lB.wpssooFSFl8v3jCP0tu4gwzOAHC.YF6"
 
 
 
